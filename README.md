838548acde785e87780b60fc23c8bb304da1c281
venus713 <userstar713@gmail.com>
2021-12-05 22:42:42 +0800

    update readme and update info function

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # logging-client
+
+`logging-client` is a Python client for logging message in a Python applications.
+`logging-client` is one part of our `Logging System`.
+
+## Installation
+
+### Minimum Requirements
+
+- python3.x
+
+### Installing with `pip`
+
+    ```
+    $ pip install git+git://github.com/Venus713/logging-module.git
+    ```
+
+### `Logger` object
+
+`Logger` object has the following attributes:
+
+- app_id: a unique identifier for an application
+- app_version_id: a unique identifier for a version of that application
+- device_id: a unique identifier for the device executing the application
+- amqp_url: a rabbitmq broker url
+
+### How to use it?
+
+    ```
+    logger = Logger(
+        app_id=<your app id>,
+        app_version_id=<your app version>,
+        device_id=<your device id>,
+        amqp_url=<your broker url>
+    )
+    logger.info(request, <your message text>, <your note>)
+    ```
--- logging_client/main.py
+++ logging_client/main.py
@@ -9,25 +9,22 @@ q = Queue()
 
 
 class Logger(object):
-    def __init__(
-        self, app_id, app_version_id, device_id, amqp_url, tbd=0, note=None
-    ) -> None:
+    def __init__(self, app_id, app_version_id, device_id, amqp_url, tbd=0,) -> None:
         self.app_id = app_id
         self.app_version_id = app_version_id
         self.device_id = device_id
         self.amqp_url = amqp_url
-        self.note = note
 
         self.context = {
             "app_id": self.app_id,
             "app_version_id": self.app_version_id,
             "device_id": self.device_id,
-            "note": self.note,
         }
         self.thread_1 = PrimaryThread(q, self.amqp_url)
 
-    def info(self, request, msg, *args, **kwargs):
+    def info(self, request, msg, note=None, *args, **kwargs):
         self.context["log_msg"] = msg
+        self.context["note"] = note
         try:
             user_id = request.user.id
         except Exception:
